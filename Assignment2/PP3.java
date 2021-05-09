8b26255d23c9f7538323d1d108cdef63efdfc227
Shikha Dongre <57437576+shikha1998@users.noreply.github.com>
2021-05-10 00:28:00 +0530

    Add files via upload

---
+++ Assignment2/PP3.java
@@ -0,0 +1,22 @@
+class PP3
+{
+	public static void main(String args[])
+	{
+		for(int i = 1; i<=9; i++)
+		{
+			for(int j = 9; j>=i; j--)
+			{
+				System.out.print(" ");
+			}
+			for(int k = 1; k<=i; k++)
+			{
+				System.out.print("*");
+			}
+			for(int l = 2; l <=i; l++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+	}
+}
