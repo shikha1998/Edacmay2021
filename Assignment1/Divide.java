de12e6dae8299b00684de884bcacae346b8a1f8a
Shikha Dongre <57437576+shikha1998@users.noreply.github.com>
2021-06-22 00:16:34 +0530

    Delete Assignment1 directory

--- Assignment1/Divide.java
+++
@@ -1,9 +0,0 @@
-class Divide
-{
-	public static void main(String args[])
-	{
-		int a = 50, b = 3;
-		int res = a/b;
-		System.out.println(res);
-	}
-}
--- Assignment1/Ten.java
+++
@@ -1,8 +0,0 @@
-class Ten
-{
-	public static void main(String args[])
-	{
-		double a = 4.0 * (1-(1.0/3) + (1.0/5) - (1.0/7) + (1.0/9) - (1.0/11));
-		System.out.println(a);
-	}
-}
